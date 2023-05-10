bbe34e23d1d705c2f545a43f91dca7d447a672dd
Sergey Kolbasin <georgio1997@mail.ru>
2023-05-11 01:05:12 +0300

    доработки

--- handlers/custom_handlers/who_born.py
+++ handlers/custom_handlers/who_born.py
@@ -6,13 +6,12 @@ from dateutil.relativedelta import relativedelta
 
 birthday = date(2023, 4, 25)
 current_day = date.today()
-
-differense = relativedelta(current_day, birthday)
+difference = current_day - birthday
 
 @Mr_Butler.message_handler(commands=['Кто_родился_у_Сергея'])
 async def hi(message: message):
     await bot.send_photo(message.from_user.id,
                          photo=open('foto/baby.jpg', 'rb'),
                          caption=f'Родилась девочка, зовут Алина 👶\n'
-                                 f'Сегодня {current_day}, а значит Алине сейчас {differense} дней',
+                                 f'Сегодня {current_day}, а значит Алине сейчас {difference.days} дней',
                          reply_markup=reply_kb)

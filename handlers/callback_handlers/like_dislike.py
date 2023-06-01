cfc359a0bfef2d03588027510276fa9587f1db84
Sergey Kolbasin <georgio1997@mail.ru>
2023-06-02 02:23:27 +0300

    доработки

--- handlers/callback_handlers/like_dislike.py
+++ handlers/callback_handlers/like_dislike.py
@@ -2,9 +2,16 @@ from aiogram.dispatcher.filters import Text
 from aiogram.types import CallbackQuery
 from run import Mr_Butler, bot
 
+check = True
 @Mr_Butler.callback_query_handler(Text(startswith='form'))
 async def like(callback: CallbackQuery):
-    if callback.data == 'form1':
-        await bot.answer_callback_query(callback.id, 'Спасибо. Алине будет очень приятно❤', show_alert= False)
+    global check
+    if check:
+        if callback.data == 'form1':
+            await bot.answer_callback_query(callback.id, 'Спасибо. Алине будет очень приятно❤', show_alert= False)
+            check = False
+        else:
+            await bot.answer_callback_query(callback.id, 'Поздравляю, Вы в черном списке! ', show_alert=False)
+            check = False
     else:
-        await bot.answer_callback_query(callback.id, 'Поздравляю, Вы в черном списке! ', show_alert=False)
+        await bot.answer_callback_query(callback.id, 'Вы уже проголосовали ', show_alert=False)
